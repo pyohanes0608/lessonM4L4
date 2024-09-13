3ea5d50dcefa48e118ed7c939a2f3b6e984272b2
Paulus <143081320+pyohanes0608@users.noreply.github.com>
2024-09-13 20:43:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -31,9 +31,5 @@ def feedback_form():
     return render_template('feedback.html', email=email, text=text)
 
 
-#if __name__ == "__main__":
-#    app.run(debug=True)
-
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    app.run(debug=True)
