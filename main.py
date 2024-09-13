d507d113f8cf713bcde94f7b0f95b3ed6796f5e7
Paulus <143081320+pyohanes0608@users.noreply.github.com>
2024-09-13 20:42:53 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+#Импорт
+from flask import Flask, render_template,request, redirect
+
+
+
+app = Flask(__name__)
+
+#Halaman Konten Berjalan
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+#Keterampilan Dinamis
+@app.route('/', methods=['POST'])
+def process_form():
+    button_python = request.form.get('button_python')
+    button_discord = request.form.get('button_discord')
+    return render_template('index.html', button_python=button_python, button_discord=button_discord)
+    
+
+# Form feedback
+@app.route('/submit', methods=['GET','POST'])
+def feedback_form():
+    email = request.form['email']
+    text = request.form['text']
+	# Kamu bisa menyimpan data atau mengirimkannya melalui email
+    with open('form.txt', 'a',) as f:
+        f.write(email + '\n')
+        f.write(text + '\n')
+    return render_template('feedback.html', email=email, text=text)
+
+
+#if __name__ == "__main__":
+#    app.run(debug=True)
+
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
